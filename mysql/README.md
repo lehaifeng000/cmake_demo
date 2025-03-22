76a9fcef4a6af6ec428b4b8aaa292dc6624e837b
lehaifeng000 <lehaifeng000@163.com>
2025-03-22 10:58:29 +0800

    添加mysql demo

---
+++ mysql/README.md
@@ -0,0 +1,2 @@
+# mysql示例
+- 使用libmysql库，官方MySQL C API
---
+++ mysql/main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <mysql/mysql.h>
+int main()
+{
+    MYSQL* conn;
+    MYSQL_RES* res;
+    MYSQL_ROW row;
+
+    conn = mysql_init(NULL);
+
+    if (conn == NULL) {
+        fprintf(stderr, "mysql_init failed\n");
+        return 1;
+    }
+
+    if (mysql_real_connect(conn, "localhost", "root", "lhf123", "demo1", 0, NULL, 0) == NULL) {
+        fprintf(stderr, "mysql_real_connect failed: %s\n", mysql_error(conn));
+        mysql_close(conn);
+        return 1;
+    }
+
+    if (mysql_query(conn, "SELECT * FROM user")) {
+        fprintf(stderr, "mysql_query failed: %s\n", mysql_error(conn));
+        mysql_close(conn);
+        return 1;
+    }
+
+    res = mysql_store_result(conn);
+
+    while ((row = mysql_fetch_row(res)) != NULL) {
+        printf("%s %s %s %s\n", row[0], row[1], row[2], row[3]);
+    }
+
+    mysql_free_result(res);
+    mysql_close(conn);
+
+    return 0;
+
+}
