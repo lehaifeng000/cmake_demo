703d854b7fdfe81cad5c51f954fda5ccae74a357
lehaifeng000 <lehaifeng000@163.com>
2025-03-24 10:15:56 +0800

    使用模板改进

--- sort/main.cpp
+++ sort/main.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 #include <vector>
 
-
-void fastSort(std::vector<int> &arr) {
+template<typename T>
+void fastSort(std::vector<T> &arr) {
 	// 用来存待排序的区间，后进先出
 	std::vector<std::pair<int, int>> stk;// 使用vector模拟栈，也可以直接用stack
 	stk.push_back(std::make_pair(0, arr.size() - 1));
@@ -11,7 +11,7 @@ void fastSort(std::vector<int> &arr) {
 		std::pair<int, int> p = stk.back();
 		stk.pop_back();
 		int l = p.first, r = p.second;// 区间的左右端点
-		int pivot = arr[r];// 选取区间的最后一个元素作为基准
+		T pivot = arr[r];// 选取区间的最后一个元素作为基准
 		int i = l - 1; // i代表已经确认小于基准的区间的最后一个元素
 		for (int j = l; j < r; j++) {
 			if (arr[j] < pivot) {
@@ -34,7 +34,8 @@ void fastSort(std::vector<int> &arr) {
 
 int main()
 {
-	std::vector<int> arr = { 1,8,4,2,6,5,0,9,3,7,2};
+	//std::vector<int> arr = { 1,8,4,2,6,5,0,9,3,7,2};
+	std::vector<float> arr = { 1.2,8.3,4.1,2.2,6,5,0,9,3,7,2 };
 	for (auto i : arr) {
 		std::cout << i << " ";
 	}
